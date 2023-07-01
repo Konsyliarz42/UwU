6da2d43e019635b8c3b952e0381b1f7b5261595c
Tomasz Kordiak <dotomika@gmail.com>
2023-07-02 00:25:54 +0200

    Fix script (#1)

--- install.sh
+++ install.sh
@@ -8,14 +8,18 @@ ICON=~/Documents/uwu/uwu.png
 SOUNDS=~/Documents/uwu/sounds
 
 echo Create desktop entry
-cp $CURRENT_DIRECTORY/uwu.desktop.template $DEKTOP_ENTRY
+cp "$CURRENT_DIRECTORY"/uwu.desktop.template $DEKTOP_ENTRY
 sed -i "s|<exec>|$EXEC|" $DEKTOP_ENTRY
 sed -i "s|<icon>|$ICON|" $DEKTOP_ENTRY
 
 echo Coping files to: ~/Documents/uwu
 mkdir ~/Documents/uwu
-cp $CURRENT_DIRECTORY/uwu.sh $EXEC
-cp $CURRENT_DIRECTORY/uwu.png $ICON
-cp -r $CURRENT_DIRECTORY/sounds $SOUNDS
+cp "$CURRENT_DIRECTORY"/uwu.sh $EXEC
+cp "$CURRENT_DIRECTORY"/uwu.png $ICON
+cp -r "$CURRENT_DIRECTORY"/sounds $SOUNDS
+
+echo Adding required permisions
+sudo chmod +x $EXEC
+sudo chmod +x $DEKTOP_ENTRY
 
 echo Success!
--- uwu.sh
+++ uwu.sh
@@ -2,10 +2,10 @@
 set -Eeuo pipefail
 
 CURRENT_DIRECTORY=$( dirname -- "$( readlink -f -- "$0"; )"; )
-VOLUME=$(amixer get Master | egrep -o '[0-9]{1,3}%')
+VOLUME=$(amixer get Master | grep -E -o -m 1 '[0-9]{1,3}%')
 
-ls $CURRENT_DIRECTORY/sounds > $CURRENT_DIRECTORY/sounds.txt
+ls "$CURRENT_DIRECTORY"/sounds > "$CURRENT_DIRECTORY"/sounds.txt
 
-amixer sset 'Master' 100%
-mpv ${CURRENT_DIRECTORY}/sounds/$(shuf -n 1 $CURRENT_DIRECTORY/sounds.txt)
-amixer sset 'Master' $VOLUME
+amixer set Master 100%
+mpv "${CURRENT_DIRECTORY}"/sounds/"$(shuf -n 1 "$CURRENT_DIRECTORY"/sounds.txt)"
+amixer set Master "$VOLUME"
